aeecc825f7257ac122f6f53eef2fa949638c439e
suunmiu <153590436+suunmiu@users.noreply.github.com>
2024-04-21 11:56:09 +0500

    Update error11.py

--- Gamejam/codes/error11.py
+++ Gamejam/codes/error11.py
@@ -29,31 +29,32 @@ y2_enemy = 600
 
 # Загрузка изображений
 hearts = [
-    pygame.transform.scale(pygame.image.load("image/hearts/xp0.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp1.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp2.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp3.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp4.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp5.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp6.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp7.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp8.png"), (150, 30)),
-    pygame.transform.scale(pygame.image.load("image/hearts/xp9.png"), (150, 30)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xpfull.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp1.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp2.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp3.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp4.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp5.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp6.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp7.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp8.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp9.png"), (320, 60)),
+    pygame.transform.scale(pygame.image.load("image/hearts/xp10.png"), (320, 60)),
 ]
 
 playerRun = [
-    pygame.transform.scale(pygame.image.load("image/run/run1.png"), (70, 70)),
-    pygame.transform.scale(pygame.image.load("image/run/run2.png"), (70, 70))
+    pygame.transform.scale(pygame.image.load("image/run/run1.png"), (100, 80)),
+    pygame.transform.scale(pygame.image.load("image/run/run2.png"), (100, 80))
 ]
 
 enemyRun = [
-    pygame.transform.scale(pygame.image.load("image/enemy/enemy1.png"), (70, 70)),
-    pygame.transform.scale(pygame.image.load("image/enemy/enemy2.png"), (70, 70))
+    pygame.transform.scale(pygame.image.load("image/enemy/enemy1.png"), (90, 85)),
+    pygame.transform.scale(pygame.image.load("image/enemy/enemy2.png"), (90, 85))
 ]
 
 playerHit = [
-    pygame.transform.scale(pygame.image.load("image/hit/hit1.png"), (70, 70)),
-    pygame.transform.scale(pygame.image.load("image/hit/hit2.png"), (70, 70))
+    pygame.transform.scale(pygame.image.load("image/hit/hit1.png"), (100, 80)),
+    pygame.transform.scale(pygame.image.load("image/hit/hit2.png"), (100, 80))
 ]
 
 reverse_playerRun = [
