7dfc7e303ed211da4ae3a47ec327441495d6467a
Rasul <110603633+I-am-King-of-the-World@users.noreply.github.com>
2024-04-22 15:13:18 +0500

    Update UI.py

--- Gamejam/codes/UI.py
+++ Gamejam/codes/UI.py
@@ -24,3 +24,24 @@ class Button:
             if self.rect.collidepoint(event.pos):
                 self.visible = False
                 pygame.display.flip()  # Обновляем экран после изменения видимости
+
+class Lose:
+    anim=0
+    restart_clicked=True
+    def Menu(self, screen):
+            
+            lose_image =pygame.transform.scale(pygame.image.load("image/UI/lose.png"), (800, 600))
+            losebutton_image =pygame.transform.scale(pygame.image.load("image/UI/losebutton.png"), (288, 72))
+            
+            screen.blit(lose_image, (0, 0))
+            screen.blit(losebutton_image, (260, 300))
+    def __init__(self, visible):
+        self.visible = visible
+        self.rect = pygame.Rect(260, 300, 260, 90)  # Пример прямоугольной области для кнопки
+    
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.restart_clicked = False
+                pygame.display.flip()  # Обновляем экран после изменения видимости
+
