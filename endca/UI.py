e9b82c2285a8681f91903cc705b3a2ca563f4929
Rasul <110603633+I-am-King-of-the-World@users.noreply.github.com>
2024-05-04 21:32:04 +0500

    versia 1.0 (final)
    this is the final version of the game and will be no update after this

--- endca/UI.py
+++ endca/UI.py
@@ -1,5 +1,66 @@
 import pygame
 
+
+class cutscen:
+     
+     def __init__(self,cutscene_bool):
+         self.cutscene_frame=0
+         self.font3=pygame.font.Font("Pokemon GB.ttf", 17)
+         self.font1=pygame.font.Font("Pokemon GB.ttf", 32)
+         self.font2=pygame.font.Font("Pokemon GB.ttf", 64)
+         self.font1_move=600
+         self.cutscene_bool=cutscene_bool
+     def scene(self,screen,cutscene):
+        self.cutscene_frame += 1
+        if self.cutscene_frame%3==0 and self.cutscene_frame%2!=0 and self.cutscene_frame<100:
+            screen.blit(cutscene[0], (0, 0))
+            text1 = self.font1.render('Attention! Floods have begun across the country, and the cause is unknown.', 1, (0, 0, 0))
+            screen.blit(text1, (self.font1_move, 550))
+            self.font1_move-=60
+        elif self.cutscene_frame%6==0 and self.cutscene_frame<100:
+            screen.blit(cutscene[1], (0, 0))
+            text1 = self.font1.render('Attention! Floods have begun across the country, and the cause is unknown.', 1, (0, 0, 0))
+            screen.blit(text1, (self.font1_move, 550))
+            self.font1_move-=60
+        if self.cutscene_frame>100:
+            screen.blit(cutscene[2], (0, 0))
+        if self.cutscene_frame>103 :
+            screen.blit(cutscene[3], (0, 0))
+        if self.cutscene_frame>106:
+            screen.blit(cutscene[4], (0, 0))
+            text1 = self.font2.render('WHAT?', 4, (0, 0, 0))
+            screen.blit(text1, (480, 520))
+        if self.cutscene_frame>118 :
+            screen.blit(cutscene[5], (0, 0))
+            text1 = self.font2.render('WHAT?', 4, (0, 0, 0))
+            screen.blit(text1, (480, 520))
+        if self.cutscene_frame>121:
+            screen.blit(cutscene[6], (0, 0))
+            text1 = self.font2.render('WHAT?', 4, (0, 0, 0))
+            screen.blit(text1, (480, 520))
+        if self.cutscene_frame>124:
+            screen.blit(cutscene[7], (0, 0))
+            text1 = self.font2.render('WHAT?', 4, (0, 0, 0))
+            screen.blit(text1, (480, 520))
+        if self.cutscene_frame>127 :
+            screen.blit(cutscene[8], (0, 0))
+        if self.cutscene_frame>130 :
+            screen.blit(cutscene[9], (0, 0))
+        if self.cutscene_frame>133 :
+            screen.blit(cutscene[10], (0, 0))
+        if self.cutscene_frame>136 :
+            screen.blit(cutscene[11], (0, 0))
+        if self.cutscene_frame>137 :
+            screen.blit(cutscene[12], (0, 0))
+        if self.cutscene_frame>140 and self.cutscene_frame<155:
+            screen.blit(cutscene[13], (0, 0))
+            text1 = self.font3.render('WHAT IS THAT?', 4, (0, 0, 0))
+            screen.blit(text1, (220, 150))
+        if self.cutscene_frame>155:
+            self.cutscene_frame=0
+            self.font1_move=600
+        return self.cutscene_bool
+
 class Button:
     anim=0
     def Menu(self, screen):
